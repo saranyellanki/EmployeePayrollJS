68251c6361f1b23bbf5b31406561ef5ca5e0748f
saranyellanki <saran.yellanki12@gmail.com>
2021-12-05 09:35:05 +0530

    Added: Function to calculate daily wage

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -4,18 +4,22 @@ const FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIMR_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-let totalEmpWage = 0;
-let attendence = Math.floor(Math.random()*3);
-switch(attendence){
-    case PART_TIME : 
-        console.log("Employee is Present PART TIME");
-        totalEmpWage = PART_TIME_HOURS*WAGE_PER_HOUR;
-        break;
-    case FULL_TIME :
-        console.log("Employee is Present FULL TIME");
-        totalEmpWage = FULL_TIMR_HOURS*WAGE_PER_HOUR;
-        break;
-    default : 
-        console.log("Employee is Absent");
+let empHrs = 0;
+function calculateWage(empHrs){
+    return WAGE_PER_HOUR * empHrs
 }
-console.log("Total Wage = "+totalEmpWage);
+function totalEmpWage(){
+    let attendence = Math.floor(Math.random()*3);
+    switch(attendence){
+        case PART_TIME : 
+            console.log("Employee is Present PART TIME");
+            return calculateWage(PART_TIME_HOURS);
+        case FULL_TIME : 
+            console.log("Employee is Present FULL TIME");
+            return calculateWage(FULL_TIMR_HOURS);
+        default : 
+            console.log("Employee is Absent");
+            return ABSENT;
+    }
+}
+console.log("Total Wage of Employee = "+totalEmpWage());
