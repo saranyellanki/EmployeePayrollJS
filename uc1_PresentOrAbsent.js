f183bc8c20686ad6358686139137cf2d2614bc2d
saranyellanki <saran.yellanki12@gmail.com>
2021-12-05 09:11:30 +0530

    Added: Employee Present or Absent

---
+++ uc1_PresentOrAbsent.js
@@ -0,0 +1,8 @@
+const PRESENT = 0;
+let attendence = Math.floor(Math.random()*2);
+if(attendence==PRESENT){
+    console.log("Employee is Present");
+}
+else{
+    console.log("Employee is Absent");
+}
