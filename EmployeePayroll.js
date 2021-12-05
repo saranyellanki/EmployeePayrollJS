abb40bac881f443bb570e557166cd981ed735f42
saranyellanki <saran.yellanki12@gmail.com>
2021-12-05 09:23:57 +0530

    Added: Daily Wage of the employee

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,8 +1,21 @@
-const PRESENT = 0;
-let attendence = Math.floor(Math.random()*2);
-if(attendence==PRESENT){
-    console.log("Employee is Present");
+const ABSENT = 0;
+const PART_TIME = 1;
+const FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIMR_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let totalEmpWage = 0;
+let attendence = Math.floor(Math.random()*3);
+switch(attendence){
+    case PART_TIME : 
+        console.log("Employee is Present PART TIME");
+        totalEmpWage = PART_TIME_HOURS*WAGE_PER_HOUR;
+        break;
+    case FULL_TIME :
+        console.log("Employee is Present FULL TIME");
+        totalEmpWage = FULL_TIMR_HOURS*WAGE_PER_HOUR;
+        break;
+    default : 
+        console.log("Employee is Absent");
 }
-else{
-    console.log("Employee is Absent");
-}
+console.log("Total Wage = "+totalEmpWage);
